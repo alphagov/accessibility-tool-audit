d45e71055861a040f494be49c4163caa9f815e0c
Anika Henke <anika.henke@digital.cabinet-office.gov.uk>
2017-11-15 15:11:54 +0000

    Fix 404 by adding missing demo.html and adjust URLs

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,6 @@
 $(function() {
     $('#webchat').click(function(){
-        window.open('demo.html','webchat', 'width=200, height=200');
+        window.open('example-pages/demo.html','webchat', 'width=200, height=200');
     });
 
     $('a.trap').keydown(function(event){
